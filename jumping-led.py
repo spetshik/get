5362594475d0acebfd8431849e089df4b8bebca5
spetshik <sahapet2009@gmail.com>
2026-09-15 19:15:07 +0300

    огонек2

--- jumping-led.py
+++ jumping-led.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-leds=[24, 22, 23,27,17,25,5,16]
+leds=[24, 22, 23,27,17,25,12,16]
 GPIO.setup(leds, GPIO.OUT)
 GPIO.output(leds, 0)
 light_time =0.2
